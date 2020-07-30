9693dd5d360f4751064558ee6c13d8a17409ab7a
Juan C <juanceballos@pursuit.org>
2020-07-30 19:41:18 -0400

    first commit -m

---
+++ ARTvel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARTvel
+//
+//  Created by Juan Ceballos on 7/29/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
