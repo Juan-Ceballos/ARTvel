dd0f3f52b206e9a44ead0dc83371093b0a30d62f
Juan C <juanceballos@pursuit.org>
2020-08-19 17:11:46 -0400

    round login button

--- ARTvel/Views/LoginView.swift
+++ ARTvel/Views/LoginView.swift
@@ -11,12 +11,26 @@ import SnapKit
 
 class LoginView: UIView {
     
-    //let bottomLine = CALayer()
+    override func layoutSubviews() {
+        loginButton.clipsToBounds = true
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.cornerRadius = loginButton.frame.size.height / 2
+        loginButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    public var appTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ARTvel"
+        label.textAlignment = .center
+        label.font = .italicSystemFont(ofSize: 33)
+        label.textColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 1)
+        return label
+    }()
     
     public var usernameTextField: UITextField = {
         let textField = UITextField()
         textField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        textField.textAlignment = .center
+        //textField.textAlignment = .center
         textField.autocorrectionType = .no
         textField.autocapitalizationType = .none
         textField.borderStyle = .none
@@ -24,7 +38,7 @@ class LoginView: UIView {
         let ca = CALayer()
         ca.frame = CGRect(x: 0, y: textField.frame.height + 22, width: UIScreen.main.bounds.size.width - 44, height: 10)
         ca.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 0.5).cgColor
-        ca.cornerRadius = ca.frame.size.width / 5
+        ca.cornerRadius = ca.frame.size.width / 200
         textField.layer.addSublayer(ca)
         return textField
     }()
@@ -33,7 +47,7 @@ class LoginView: UIView {
         let textField = UITextField()
         textField.attributedPlaceholder = NSAttributedString(string: "Password",
         attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        textField.textAlignment = .center
+        //textField.textAlignment = .center
         textField.autocorrectionType = .no
         textField.autocapitalizationType = .none
         textField.borderStyle = .none
@@ -41,26 +55,15 @@ class LoginView: UIView {
         let ca = CALayer()
         ca.frame = CGRect(x: 0, y: textField.frame.height + 22, width: UIScreen.main.bounds.size.width - 44, height: 10)
         ca.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 0.5).cgColor
-        ca.cornerRadius = ca.frame.size.width / 5
+        ca.cornerRadius = ca.frame.size.width / 200
         textField.layer.addSublayer(ca)
         return textField
     }()
     
     public var loginButton: UIButton = {
-        let button = UIButton(type: .roundedRect)
+        let button = UIButton()
         button.setTitle("Create My Account", for: .normal)
-        //button.backgroundColor = .systemGray5
         return button
-    }()
-    
-    public var appTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "ARTvel"
-        label.textAlignment = .center
-        label.font = .italicSystemFont(ofSize: 33)
-        label.textColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 1)
-        //label.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 0.5)
-        return label
     }()
     
     override init(frame: CGRect) {
