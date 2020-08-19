bfd2e25aeedccd45eb70705fd7dac7b67b09613b
Juan C <juanceballos@pursuit.org>
2020-08-19 16:34:16 -0400

    adjusted textfield line, title, create account button, ui colors for loginVC

--- ARTvel/Views/LoginView.swift
+++ ARTvel/Views/LoginView.swift
@@ -15,42 +15,51 @@ class LoginView: UIView {
     
     public var usernameTextField: UITextField = {
         let textField = UITextField()
-        textField.attributedPlaceholder = NSAttributedString(string: "username",
-        attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        textField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         textField.textAlignment = .center
         textField.autocorrectionType = .no
         textField.autocapitalizationType = .none
         textField.borderStyle = .none
+        textField.textColor = .white
         let ca = CALayer()
-        ca.frame = CGRect(x: 0, y: textField.frame.height + 22, width: UIScreen.main.bounds.size.width - 44, height: 1)
-        ca.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 1).cgColor
+        ca.frame = CGRect(x: 0, y: textField.frame.height + 22, width: UIScreen.main.bounds.size.width - 44, height: 10)
+        ca.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 0.5).cgColor
+        ca.cornerRadius = ca.frame.size.width / 5
         textField.layer.addSublayer(ca)
         return textField
     }()
     
     public var passwordTextField: UITextField = {
         let textField = UITextField()
-        textField.attributedPlaceholder = NSAttributedString(string: "password",
+        textField.attributedPlaceholder = NSAttributedString(string: "Password",
         attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         textField.textAlignment = .center
         textField.autocorrectionType = .no
         textField.autocapitalizationType = .none
         textField.borderStyle = .none
+        textField.textColor = .white
         let ca = CALayer()
-        ca.frame = CGRect(x: 0, y: textField.frame.height + 22, width: UIScreen.main.bounds.size.width - 44, height: 1)
-        ca.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 1).cgColor
+        ca.frame = CGRect(x: 0, y: textField.frame.height + 22, width: UIScreen.main.bounds.size.width - 44, height: 10)
+        ca.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 0.5).cgColor
+        ca.cornerRadius = ca.frame.size.width / 5
         textField.layer.addSublayer(ca)
         return textField
     }()
     
     public var loginButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .roundedRect)
+        button.setTitle("Create My Account", for: .normal)
+        //button.backgroundColor = .systemGray5
         return button
     }()
     
     public var appTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "ARTvel"
+        label.textAlignment = .center
+        label.font = .italicSystemFont(ofSize: 33)
+        label.textColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 1)
+        //label.backgroundColor = UIColor(red: 166/255, green: 125/255, blue: 39/255, alpha: 0.5)
         return label
     }()
     
@@ -65,14 +74,25 @@ class LoginView: UIView {
     }
     
     private func commonInit()   {
+        setupAppTitleLabelConstraints()
         setupUsernameTextFieldConstraints()
         setupPasswordTextFieldConstraints()
+        setupLoginButtonConstraints()
+    }
+    
+    private func setupAppTitleLabelConstraints()    {
+        addSubview(appTitleLabel)
+        appTitleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(44)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+        }
     }
     
     private func setupUsernameTextFieldConstraints()    {
         addSubview(usernameTextField)
         usernameTextField.snp.makeConstraints { (make) in
-            make.centerY.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-66)
             make.left.equalToSuperview().offset(22)
             make.right.equalToSuperview().offset(-22)
         }
@@ -81,9 +101,18 @@ class LoginView: UIView {
     private func setupPasswordTextFieldConstraints()    {
         addSubview(passwordTextField)
         passwordTextField.snp.makeConstraints { (make) in
-            make.top.equalTo(usernameTextField.snp.bottom).offset(22)
+            make.top.equalTo(usernameTextField.snp.bottom).offset(33)
             make.left.equalToSuperview().offset(22)
             make.right.equalToSuperview().offset(-22)
+        }
+    }
+    
+    private func setupLoginButtonConstraints()  {
+        addSubview(loginButton)
+        loginButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(33)
+            make.left.equalToSuperview().offset(33)
+            make.right.equalToSuperview().offset(-33)
         }
     }
     
